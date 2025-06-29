913a31a4c0cbfa610e1bda2122306ae56ac19065
ooibowei <ooibowei@gmail.com>
2025-06-30 01:46:00 +0800

    feat: complete pose estimation on data set

---
+++ scripts/data_augmentation.py
@@ -0,0 +1,8 @@
+"""
+Augment keypoints training set
+- Add small random noise (jitter)
+- Random rotation (around center)
+- Random scaling
+- Random translation
+- Random dropout (simulate missing landmarks)
+"""
--- scripts/dataset_preprocessing.py
+++ scripts/dataset_preprocessing.py
@@ -1,40 +1,49 @@
+# Extract and normalise keypoints. Split into train/val/test
+
 import cv2
+import os
 import mediapipe as mp
+import pandas as pd
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
-mp_pose = mp.solutions.pose
-pose_image = mp_pose.Pose(static_image_mode=True)
+# Use heavy model to train pose classifier
+model_path = 'models/pose_landmarker_heavy.task'
+base_options = python.BaseOptions(model_asset_path=model_path)
+options = vision.PoseLandmarkerOptions(base_options=base_options)
+pose_landmarker = vision.PoseLandmarker.create_from_options(options)
 
-image_path = 'data/raw/Akarna_Dhanurasana/Akarna_Dhanurasana_image_1.jpg'
-image = cv2.imread(image_path)
-image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-res = pose_image.process(image_rgb)
+data_dir = 'data/raw'
+rows = []
+colnames = ['label'] + [f'{dim}{i}' for i in range(33) for dim in ('x','y','z','v')]
+for dirpath, dirnames, filenames in os.walk(data_dir):
+    for filename in filenames:
+        if not filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+            continue
 
-keypoints = [] # (x1, y1, z1, v1, x2, y2, z2,...)
-for lm in res.pose_landmarks.landmark:
-    keypoints.extend([lm.x, lm.y, lm.z, lm.visibility])
+        image_path = os.path.join(dirpath, filename)
+        image = cv2.imread(image_path)
+        if image is None:
+            continue
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing.draw_landmarks(
-    image,
-    res.pose_landmarks,
-    mp_pose.POSE_CONNECTIONS,
-    landmark_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
-    connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2)
-)
-for idx, landmark in enumerate(res.pose_landmarks.landmark):
-    h, w, _ = image.shape
-    x, y = int(landmark.x * w), int(landmark.y * h)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
+        pose_res = pose_landmarker.detect(mp_image)
+        if not pose_res.pose_landmarks:
+            continue
 
-    cv2.putText(
-        image,
-        f'{mp_pose.PoseLandmark(idx).name}',
-        (x + 5, y - 5),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.4,
-        (255, 255, 255),
-        1,
-        cv2.LINE_AA
-    )
-cv2.imshow('Pose Landmarks', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+        keypoints = []  # (x1, y1, z1, v1, x2, y2, z2,...)
+        for lm in pose_res.pose_landmarks[0]:
+            keypoints.extend([lm.x, lm.y, lm.z, lm.visibility])
+        pose_name = os.path.basename(dirpath)
+        row = [pose_name] + keypoints
+        rows.append(row)
+
+df = pd.DataFrame(rows, columns=colnames).drop_duplicates()
+df.to_parquet('data/processed/df.parquet')
+
+"""
+Normalize keypoints wrt reference landmark (eg hip)
+Decide what to do with z-coord
+Train/val/test split
+"""

6f4cfc8c6892342d7c5ff46defefebc2a04eb706
ooibowei <ooibowei@gmail.com>
2025-07-01 03:11:00 +0800

    feat: apply normalisation to augmented data

--- reports/01_modelling.md
+++ reports/01_modelling.md
@@ -6,9 +6,8 @@ We XX
 
 ## Data Augmentation
 
-To improve the training set, we augmented the keypoints using the following:
-- Add small random noise (jitter) between x and x
-- Random rotation around center between x and x
-- Random scaling between x and x
-- Random translation between x and x
-- Random dropout (simulate missing landmarks)
+To improve model generalisability and robustness, we augmented the training set by applying the following procedure 3 separate times to each training set pose. These augmentations reflect real-world variations like different camera angles and pose imperfections
+- Added small random Gaussian noise (jitter) drawn from a N(0, 0.0001)
+- Rotated around the center by -10 to 10 degrees
+- Randomly dropped landmarks with probability 0.05
+ 
--- scripts/data_augmentation.py
+++ scripts/data_augmentation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scripts.utils import normalise_keypoints
 
 def jitter_keypoints(arr, sigma):
     """
@@ -13,36 +14,6 @@ def jitter_keypoints(arr, sigma):
     """
     noise = np.random.normal(0, sigma, arr.shape)
     return arr + noise
-
-def scale_keypoints(arr, scale_range):
-    """
-    Scale each keypoint by Uniform(-scale_range, scale_range)
-    :param arr: Array of keypoints
-    :type arr: numpy.ndarray of shape (33, 4)
-    :param scale_range: Amount of scaling to apply
-    :type scale_range: float
-    :return: Array of keypoints with scaled values
-    :rtype: numpy.ndarray of shape (33, 4)
-    """
-    scale = np.random.uniform(1-scale_range, 1+scale_range)
-    ctr = np.mean(arr[:, :2], axis=0) # (mean_x, mean_y)
-    arr[:, :2] = ((arr[:, :2] - ctr) * scale) + ctr # make the ctr origin, scale, then shift back
-    return arr
-
-def translate_keypoints(arr, trans_range):
-    """
-    Translate each keypoint by Uniform(-trans_range, trans_range)
-    :param arr: Array of keypoints
-    :type arr: numpy.ndarray of shape (33, 4)
-    :param trans_range: Amount of transformation to apply
-    :type trans_range: float
-    :return: Array of keypoints with transformed values
-    :rtype: numpy.ndarray of shape (33, 4)
-    """
-    dx, dy = np.random.uniform(-trans_range, trans_range, 2)
-    arr[:,0] += dx
-    arr[:,1] += dy
-    return arr
 
 def rotate_keypoints(arr, angle_range):
     """
@@ -87,12 +58,11 @@ for i in range(len(x_train)):
     label = y_train.iloc[i]
 
     for j in range(3):
-        jitter_keypoints(arr, 0.01)
-        scale_keypoints(arr, 0.05)
-        translate_keypoints(arr, 0.05)
-        rotate_keypoints(arr, 10)
-        dropout_keypoints(arr, 0.05)
-        rows_aug.append(arr.flatten().tolist())
+        arr = jitter_keypoints(arr, 0.01)
+        arr = rotate_keypoints(arr, 10)
+        arr = dropout_keypoints(arr, 0.05)
+        arr_norm = normalise_keypoints(arr)
+        rows_aug.append(arr_norm.flatten().tolist())
         labels_aug.append(label)
 
 x_aug = pd.DataFrame(rows_aug, columns=x_train.columns)
@@ -102,3 +72,35 @@ y_train_aug = pd.concat([y_train, y_aug], ignore_index=True, axis=0)
 
 x_train_aug.to_parquet('data/processed/x_train_aug.parquet')
 pd.DataFrame(y_train_aug, columns=['label']).to_parquet('data/processed/y_train_aug.parquet')
+
+'''
+def scale_keypoints(arr, scale_range):
+    """
+    Scale each keypoint by Uniform(-scale_range, scale_range)
+    :param arr: Array of keypoints
+    :type arr: numpy.ndarray of shape (33, 4)
+    :param scale_range: Amount of scaling to apply
+    :type scale_range: float
+    :return: Array of keypoints with scaled values
+    :rtype: numpy.ndarray of shape (33, 4)
+    """
+    scale = np.random.uniform(1-scale_range, 1+scale_range)
+    ctr = np.mean(arr[:, :2], axis=0) # (mean_x, mean_y)
+    arr[:, :2] = ((arr[:, :2] - ctr) * scale) + ctr # make the ctr origin, scale, then shift back
+    return arr
+
+def translate_keypoints(arr, trans_range):
+    """
+    Translate each keypoint by Uniform(-trans_range, trans_range)
+    :param arr: Array of keypoints
+    :type arr: numpy.ndarray of shape (33, 4)
+    :param trans_range: Amount of transformation to apply
+    :type trans_range: float
+    :return: Array of keypoints with transformed values
+    :rtype: numpy.ndarray of shape (33, 4)
+    """
+    dx, dy = np.random.uniform(-trans_range, trans_range, 2)
+    arr[:,0] += dx
+    arr[:,1] += dy
+    return arr
+'''
